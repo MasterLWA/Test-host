64f66145508e297f4bdcea79e53c755995d5c765
Lakindu Widuranga Alwis <29312088+MasterLWA@users.noreply.github.com>
2023-10-16 11:51:01 +0530

    frontend

--- src/Pages/Dashbord.jsx
+++ src/Pages/Dashbord.jsx
@@ -3,6 +3,7 @@ import NavButton from "../Components/NavButtons";
 const Dashbord = () => {
     return (   
         <div>
+            <NavButton />
             <div className="container">
                 dashboard
             </div>
--- src/Pages/UserSide/Mrn.jsx
+++ src/Pages/UserSide/Mrn.jsx
@@ -1,6 +1,4 @@
 import NavButton from "../../Components/NavButtons";
-import React, { useState, useEffect } from "react"
-
 
 const Mrn = () => {
     return (
