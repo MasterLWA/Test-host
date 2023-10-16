8ea56bdd855e6728bf3f3f51c9466ec0cd76dbd3
Lakindu Widuranga Alwis <29312088+MasterLWA@users.noreply.github.com>
2023-10-17 00:24:52 +0530

    h

--- src/Routers/RouterApp.jsx
+++ src/Routers/RouterApp.jsx
@@ -1,30 +1,44 @@
 import React from 'react';
 import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom';
-import Login from '../Pages/Loging'; // Corrected the import statement
+import Login from '../Pages/Loging';
 
 import Dashbord from '../Pages/Dashbord';
-import Billing from '../Pages/UserSide/Billing';
-import Grn from '../Pages/UserSide/Grn';
+
+import Billing from '../Pages/UserSide/Billing'
+import Grn from '../Pages/UserSide/Grn'
 import Editsupplier from '../Pages/UserSide/Editsupplier';
 import Editgrn from '../Pages/UserSide/Editgrn';
-import Mrn from '../Pages/UserSide/Mrn';
-import Supplier from '../Pages/UserSide/Supplier';
+import Mrn from '../Pages/UserSide/Mrn'
+import Supplier from '../Pages/UserSide/Supplier'
 
+
+
+/**
+ * Renders the application router with different routes for different pages.
+ * @returns {JSX.Element} The router component with different routes.
+ */
 const RouterApp = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<Navigate to="/login" />} />
 
         <Route path="/dashboard" element={<Dashbord />} />
+
         <Route path="/billing" element={<Billing />} />
+
         <Route path="/grn" element={<Grn />} />
         <Route path="/editgrn/:id" element={<Editgrn />} />
+
         <Route path="/mrn" element={<Mrn />} />
+
         <Route path="/suppliers" element={<Supplier />} />
         <Route path="/suppliers/:id" element={<Editsupplier />} />
+
+
       </Routes>
     </BrowserRouter>
   );
